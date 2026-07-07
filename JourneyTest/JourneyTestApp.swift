26f96e127a1a3c333c7efe0e2a295737a0821644
Justin Whitehead <justin.ray011@gmail.com>
2026-07-07 11:23:32 -0600

    Initial Commit

---
+++ JourneyTest/JourneyTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  JourneyTestApp.swift
+//  JourneyTest
+//
+//  Created by Justin Whitehead on 7/7/26.
+//
+
+import SwiftUI
+
+@main
+struct JourneyTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
